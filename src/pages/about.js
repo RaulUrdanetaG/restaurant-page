a24473ce968964c28805472083155abe30de0bd1
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2023-08-04 22:00:23 -0500

    Fixed webpack images issues for about tab

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,19 @@
 import createNavBar from '../sections/nav-bar';
 import createFooter from '../sections/footer';
 
+import mainAboutImage from '../assets/images/about-images/about-us-hero_large.jpg';
+import blackbarAbout from '../assets/images/menu-images/icons/black-bar_bg.svg';
+import blackbarAfter from '../assets/images/about-images/illustration-wheat.png';
+import historyAfter from '../assets/images/about-images/illustration-beet.png';
+import memoriesImg from '../assets/images/about-images/our-memories_medium.jpg';
+
 function createAbout() {
     const content = document.getElementById('content');
 
     const hero = document.createElement('section');
     hero.classList.add('hero-about');
+    hero.style.background = `url(${mainAboutImage}) center center no-repeat`;
+    hero.style.backgroundSize = 'cover';
     content.appendChild(hero);
 
     const titleContent = document.createElement('div');
@@ -14,6 +22,8 @@ function createAbout() {
 
     const blackbar = document.createElement('h2');
     blackbar.classList.add('black-bar-about');
+    blackbar.style.background = `url(${blackbarAbout})center center no-repeat`;
+    blackbar.style.setProperty('--blackbar-after',`url('${blackbarAfter}')`);
     blackbar.innerText = 'PAUL & JESSICA';
     titleContent.appendChild(blackbar);
 
@@ -36,10 +46,13 @@ function createAbout() {
                                   reservations at a specific restaurant. We’ve had some amazing meals and some great adventures, and it all shows 
                                   up in the food we serve.</p>`,
     historyContainer.classList.add('history-container');
+    historyContainer.style.setProperty('--history-after',`url('${historyAfter}')`);
     ourHistorySection.appendChild(historyContainer);
 
     const overlayHero = document.createElement('section');
     overlayHero.classList.add('memories-hero');
+    overlayHero.style.background = `url('${memoriesImg}') center center no-repeat`;
+    overlayHero.style.backgroundSize = 'cover';
     content.appendChild(overlayHero);
 
     const overlay = document.createElement('div');
@@ -58,7 +71,7 @@ function loadAbout() {
 
     const main = document.getElementById('content');
     main.innerHTML = '';
-    
+
     createNavBar();
     createAbout();
     createFooter();
